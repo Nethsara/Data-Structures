72ab4394c92b12051aa9614f9a792b88d844a419
Nethsara <nethsarasiyum2@gmail.com>
2023-05-29 22:10:03 +0530

    object stack, specific object type

--- stack/Demo.java
+++ stack/Demo.java
@@ -1,124 +1,149 @@
 package stack;
 
-interface StackInterface {
-    public void push(int data);
+class Customer {
+    private int code;
+    private String name;
+
+    Customer(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCustomerDetails() {
+        return code + "-" + name;
+    }
+
+    public boolean equals(Customer customer) {
+        return this.code == customer.code;
+    }
+}
+
+interface CustomerStack {
+    public void push(Customer customer);
 
     public void printStack();
 
     public void pop();
 
-    public int peek();
+    public Customer peek();
 
-    public int poll();
+    public Customer poll();
 
-    public int[] toArray();
+    public Customer[] toArray();
 
     public void clear();
 
+    public int size();
+
     public boolean isEmpty();
 
+    public int search(Customer customer);
 }
 
-class NoSuchElementException extends RuntimeException {
-    NoSuchElementException(String message) {
-        super(message);
-    }
-}
+class CustomerStackImpl implements CustomerStack {
+    private Customer[] customerArray;
+    private int nextIndex = 0;
 
-class Stack implements StackInterface {
-    private int[] dataArray;
-
-    Stack() {
-        dataArray = new int[0];
+    CustomerStackImpl() {
+        customerArray = new Customer[100];
     }
 
-    private void extendArray() {
-        int[] temp = new int[dataArray.length + 1];
-        for (int i = 0; i < dataArray.length; i++) {
-            temp[i + 1] = dataArray[i];
-        }
-        dataArray = temp;
-
-    }
-
-    public void push(int data) {
-        extendArray();
-        dataArray[0] = data;
+    public void push(Customer customer) {
+        customerArray[nextIndex++] = customer;
     }
 
     public void pop() {
         if (!isEmpty()) {
-            int[] temp = new int[dataArray.length - 1];
-            for (int i = 0; i < temp.length; i++) {
-                temp[i] = dataArray[i + 1];
-            }
-            dataArray = temp;
+            customerArray[--nextIndex] = null;
         }
     }
 
     public void printStack() {
         System.out.print("[");
-        for (int i = 0; i < dataArray.length; i++) {
-            System.out.print(dataArray[i] + ", ");
+        for (int i = nextIndex - 1; i >= 0; i--) {
+            System.out.print(customerArray[i].getCustomerDetails() + ", ");
         }
         System.out.println(isEmpty() ? "empty]" : "\b\b]");
     }
 
-    public int peek() {
+    public Customer peek() {
         if (isEmpty()) {
-            throw new NoSuchElementException("Empty Stack");
+            return null;
         }
-        return dataArray[0];
+        return customerArray[nextIndex - 1];
     }
 
-    public int poll() {
+    public Customer poll() {
         if (isEmpty()) {
-            throw new NoSuchElementException("Empty Stack");
+            return null;
         }
-        int topData = dataArray[0];
+        Customer customer = customerArray[nextIndex - 1];
         pop();
-        return topData;
+        return customer;
     }
 
     public int size() {
-        return dataArray.length;
+        return nextIndex;
     }
 
     public void clear() {
-        dataArray = new int[0];
+        nextIndex = 0;
     }
 
-    public int[] toArray() {
-        int[] temp = new int[dataArray.length];
-        for (int i = 0; i < dataArray.length; i++) {
-            temp[i] = dataArray[i];
+    public Customer[] toArray() {
+        Customer[] temp = new Customer[nextIndex];
+        for (int i = 0, j = nextIndex - 1; i < nextIndex; i++, j--) {
+            temp[i] = customerArray[j];
         }
         return temp;
     }
 
     public boolean isEmpty() {
-        return dataArray.length == 0;
+        return nextIndex == 0;
+    }
+
+    public int search(Customer customer) {
+        for (int i = 0; i < nextIndex; i++) {
+            if (customerArray[i].equals(customer)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
 
 class Demo {
     public static void main(String args[]) {
-        Stack s1 = new Stack();
-        s1.printStack(); // [empty]
-        System.out.println("Size of the stack : " + s1.size());// 0
-        s1.push(10);
-        s1.push(20);
-        s1.push(30);
-        s1.push(40);
-        s1.push(50);
+        CustomerStack custStack = new CustomerStackImpl();
+        System.out.println("Size of the stack : " + custStack.size());
+        custStack.printStack();
+        System.out.println();
 
-        s1.printStack(); // [50,40,30,20,10]
-        System.out.println("Size of the stack : " + s1.size());// 5
+        custStack.push(new Customer(1001, "Danapala"));
+        custStack.push(new Customer(1002, "Gunapala"));
+        custStack.push(new Customer(1003, "Somapala"));
+        custStack.push(new Customer(1004, "Siripala"));
+        custStack.push(new Customer(1005, "Amarapala"));
+        custStack.printStack();
+        System.out.println("Size of the stack : " + custStack.size()); // 5
+        System.out.println();
 
-        s1.clear();
-        s1.printStack(); // [empty]
-        System.out.println("Size of the stack : " + s1.size());// 0
+        custStack.pop();
+        custStack.printStack();
+        System.out.println("Size of the stack : " + custStack.size()); // 5
+        System.out.println();
 
-        s1.peek(); // throw an exception
+        Customer topCustomer = custStack.peek();
+        System.out.println("Top Customer : " + topCustomer.getCustomerDetails());
+        custStack.printStack();
+
+        System.out.println();
+        topCustomer = custStack.poll();
+        System.out.println("Top Customer : " + topCustomer.getCustomerDetails());
+        custStack.printStack();
+
+        System.out.println();
+
+        System.out.println("Index of 1002-Gunapala : " + custStack.search(new Customer(1002, "Gunapala")));
     }
 }
